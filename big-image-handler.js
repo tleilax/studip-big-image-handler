67f4b88291cebed4de5c3b1ce0b2cd26fc707c5a
Jan-Hendrik Willms <tleilax+github@gmail.com>
2014-02-10 10:35:12 +0100

    provide close icon, replace text with icon if image width is very small, close overlay on 'escape' keypress

--- big-image-handler.js
+++ big-image-handler.js
@@ -1,6 +1,6 @@
 (function ($) {
 
-$('img:not(.big-image-handled)').live('mouseenter', function () {
+$(document).on('mouseenter', 'img:not(.big-image-handled)', function () {
     var $that   = $(this),
         $parent = $that.parent(),
         src     = $that.attr('src'),
@@ -8,8 +8,7 @@ $('img:not(.big-image-handled)').live('mouseenter', function () {
         height  = parseInt($that.height(), 10),
         img;
 
-    if (width < 200
-        || $that.is('[class*=avatar][class*=user]')
+    if ($that.is('[class*=avatar][class*=user]')
         || ($parent.is('a[href]') && $parent.attr('href') !== src)) {
         return;
     }
@@ -24,6 +23,8 @@ $('img:not(.big-image-handled)').live('mouseenter', function () {
                 text += '<nobr>' + 'Klicken Sie auf das Bild, um es in Originalgröße zu öffnen.'.toLocaleString() + '</nobr>';
             } else if (width >= 300) {
                 text += '<nobr>' + 'Klicken für Originalgröße.'.toLocaleString() + '</nobr>';
+            } else if (width < 100) {
+                text = $('<img>').attr('title', text).attr('src', STUDIP.ASSETS_URL + 'images/icons/16/white/visibility-invisible.png');
             }
 
             info.addClass('big-image-info').html(text);
@@ -34,16 +35,36 @@ $('img:not(.big-image-handled)').live('mouseenter', function () {
     img.src = src;
 });
 
-$('.big-image-handler').live('mouseenter', function () {
+$(document).on('mouseenter', '.big-image-handler', function () {
     $('div', this).stop(true, true).show('slide', {direction: 'up'});
-}).live('mouseleave', function () {
+}).on('mouseleave', '.big-image-handler', function () {
     $('div', this).stop(true, true).hide('slide', {direction: 'up'});
-}).live('click', function (event) {
+}).on('click', '.big-image-handler', function (event) {
     // Open as full screen lightbox
-    var src = $('img', this).attr('src'),
-        img = $('<img>').addClass('big-image-handled').attr('src', src);
-    $('<div class="big-image-overlay">').append(img).appendTo('body').click(function () {
+    var src     = $('img', this).attr('src'),
+        img     = $('<img class="big-image-handled">'),
+        close   = $('<div class="big-image-close">'),
+        overlay = $('<div class="big-image-overlay">'),
+        offset;
+
+    img.load(function () {
+        offset = img.position();
+        close.css({
+            left: offset.left + img.width() - 18,
+            top: offset.top + 2
+        }).insertAfter(img);
+    });
+    img.attr('src', src);
+
+    overlay.append(img).appendTo('body').click(function () {
+        $(document).off('keypress.big-image');
         $(this).remove();
+    });
+    
+    $(document).on('keypress.big-image', function (event) {
+        if (event.key === 'Esc') {
+            overlay.click();
+        }
     });
 
     event.preventDefault();
